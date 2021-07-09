dd6469fa9569a5b85ca4ccbdb20883791a6c4864
Dmitry Panin <pankdm@gmail.com>
2021-07-09 12:54:07 -0700

    First solution!

--- solve.py
+++ solve.py
@@ -1,6 +1,8 @@
 import sys
 from utils import read_problem
-import shapely
+import json
+
+from shapely.geometry import Point, Polygon
 
 problem_id = sys.argv[1]
 print (f'Solving {problem_id}')
@@ -9,8 +11,15 @@ spec = read_problem(problem_id)
 print (spec)
 
 
-def is_inside(spec, x, y):
-    pass
+def is_inside(polygon, x, y):
+    pt = Point(x, y)
+    return polygon.contains(pt) or polygon.touches(pt)
+
+def dist2(pt1, pt2):
+    x1, y1 = pt1
+    x2, y2 = pt2
+    return (x1 - x2) ** 2 + (y1 - y2) ** 2
+
 
 def compute_inside_points(spec):
     res = set()
@@ -28,20 +37,34 @@ def compute_inside_points(spec):
         xmin = min(xmin, x)
         ymin = min(ymin, y)
     
+    polygon = Polygon(spec['hole'])
     for x in range(xmin, xmax + 1):
         for y in range(ymin, ymax + 1):
-            if is_inside(spec, x, y):
+            if is_inside(polygon, x, y):
                 res.add((x, y))
     return res
 
 
+def check_distance(spec, orig_dist, new_dist):
+    if abs(1.0 * new_dist / orig_dist - 1) <= spec['epsilon'] / 10**6:
+        return True
+    return False
 
 # solution = {id -> [x, y]}
 def check_partial_solution(spec, solution):
-    # TODO: implement
+    # TODO: check intersection with boundaries
+    for edge in spec['figure']['edges']:
+        a, b = edge
+        if a in solution and b in solution:
+            pt_a = solution[a]
+            pt_b = solution[b]
+            orig_dist = dist2(spec['figure']['vertices'][a], spec['figure']['vertices'][b])    
+            new_dist = dist2(pt_a, pt_b)
+            if not check_distance(spec, orig_dist, new_dist):
+                return False
     return True
 
-def try_solve(spec, solution):
+def try_solve(spec, solution, inside_points):
     ok = check_partial_solution(spec, solution)
     if not ok:
         return False
@@ -53,6 +76,8 @@ def try_solve(spec, solution):
         # swap to always treat as a in solution
         if b in solution:
             a, b = b, a
+        if a not in solution:
+            continue
         assert (a in solution)
         if b in solution:
             continue
@@ -64,6 +89,36 @@ def try_solve(spec, solution):
     if edge_to_add is None:
         return True
 
-    # find next possible points for b
+    # find next possible point for b
+    orig_dist = dist2(spec['figure']['vertices'][a], spec['figure']['vertices'][b])    
+    for pt in inside_points:
+        new_dist = dist2(solution[a], pt)
+        if check_distance(spec, orig_dist, new_dist):
+            solution[b] = pt
+            found = try_solve(spec, solution, inside_points)
+            if found:
+                return True
+            # otherwise restore previous state
+            del solution[b]
+    return False
+        
+inside = list(sorted(compute_inside_points(spec)))
+print ('inside points:', len(inside))
 
 
+first_hole_pt = spec['hole'][0]
+total_points = len(spec['figure']['vertices'])
+
+for index in range(total_points):
+    print ('Trying connecting index={} to {}'.format(index, first_hole_pt))
+    solution = {index: first_hole_pt}
+    found = try_solve(spec, solution, inside)
+    if found:
+        print ('Writing solution', solution)
+        with open(f'solutions/tmp/{problem_id}', 'w') as f:
+            vertices = []
+            for index in range(total_points):
+                vertices.append(solution[index])
+            res = {'vertices' : vertices}
+            f.write(json.dumps(res))
+        break
