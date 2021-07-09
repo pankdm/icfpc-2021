3bb4af83e5a2dc0135c1e815376f8b42f0a9fb0c
Victor Klochikhin <vintlucky777@gmail.com>
2021-07-09 14:47:43 -0700

    ui: remove debug hack

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -8,9 +8,6 @@ import { useProblem } from './api/problems'
 function App() {
   const [problemId, setProblemId] = useLocalStorage('problemId', 1)
   const { isLoading, error, data: problem } = useProblem(problemId, { enabled: !!problemId })
-  if (problem) {
-    problem.figure.vertices[0][0] = 2
-  }
   return (
     <div className={styles.app}>
       <h1>Welcome to ICFPC 2021!</h1>
