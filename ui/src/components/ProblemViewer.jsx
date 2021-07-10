61da282d3345d85ca1ad4099a8e23b6ca745a3bd
Victor Klochikhin <vintlucky777@gmail.com>
2021-07-09 20:08:28 -0700

    UI: move graph physics -> utils/physics.js

--- ui/src/components/ProblemViewer.jsx
+++ ui/src/components/ProblemViewer.jsx
@@ -6,46 +6,10 @@ import Hole from './svg/Hole.jsx'
 import Figure from './svg/Figure.jsx'
 import styles from './ProblemViewer.module.css'
 import useBlip from '../utils/useBlip.js'
-import { vecMean, vecAdd, vecSub, vecSetAbs, vecAbs, distance, vecNorm, vecMult, getDistanceMap, vecClampAbs, vecRand, isVecZero } from '../utils/graph.js'
+import { getDistanceMap } from '../utils/graph.js'
+import { relaxVertices } from '../utils/physics.js'
 import { useOnChangeValues } from '../utils/useOnChange.js'
 import useAnimLoop from '../utils/useAnimLoop.js'
-
-const getLinearFravityForce = (point, gravityCenter, gravityConst=10) => {
-  const toGravityCenter = vecSub(gravityCenter, point)
-  if (isVecZero(toGravityCenter)) {
-    return [0, 0]
-  }
-  return vecClampAbs(vecMult(toGravityCenter, gravityConst), 0, gravityConst)
-}
-const getRepelForce = (point, otherPoint, repelConst=3000, maxRepel=1000) => {
-  const fromOtherPoint = vecSub(point, otherPoint)
-  if (isVecZero(fromOtherPoint)) {
-    return vecRand(maxRepel)
-  }
-  const dist = distance(point, otherPoint)
-  return vecClampAbs(vecMult(fromOtherPoint, repelConst/dist**3), 0, maxRepel)
-}
-const getSpringForce = (point, otherPoint, optimalDistance, springConst=50) => vecMult(vecNorm(vecSub(point, otherPoint)), springConst * (1 - distance(point, otherPoint) / optimalDistance))
-
-const relaxVertices = (vertices, optimalDistancesMap, gravityCenter, timeStep=0.1) => {
-  const meanCoords = vecMean(vertices)
-  const gravity = getLinearFravityForce(meanCoords, gravityCenter)
-  const newVertices = _.map(vertices, (v, idx) => {
-    let sumForce = gravity
-    vertices.forEach((ov, ovIdx) => {
-      if (ov == v) return
-      const repelForce = getRepelForce(v, ov)
-      sumForce = vecAdd(sumForce, repelForce)
-      const optimalDistance = optimalDistancesMap[idx][ovIdx] || null
-      if (optimalDistance) {
-        const springForce = getSpringForce(v, ov, optimalDistance)
-        sumForce = vecAdd(sumForce, springForce)
-      }
-    })
-    return vecAdd(v, vecMult(sumForce, timeStep))
-  })
-  return newVertices
-}
 
 export default function ProblemViewer({ problem, solution, ...props }) {
   const [shake, toggleShake] = useBlip(300)
---
+++ ui/src/utils/physics.js
@@ -0,0 +1,38 @@
+import { vecMean, vecAdd, vecSub, distance, vecNorm, vecMult, vecClampAbs, vecRand, isVecZero } from './graph.js'
+
+export const getLinearFravityForce = (point, gravityCenter, gravityConst=100) => {
+  const toGravityCenter = vecSub(gravityCenter, point)
+  if (isVecZero(toGravityCenter)) {
+    return [0, 0]
+  }
+  return vecClampAbs(vecMult(toGravityCenter, gravityConst), 0, gravityConst)
+}
+export const getRepelForce = (point, otherPoint, repelConst=3000, maxRepel=1000) => {
+  const fromOtherPoint = vecSub(point, otherPoint)
+  if (isVecZero(fromOtherPoint)) {
+    return vecRand(maxRepel)
+  }
+  const dist = distance(point, otherPoint)
+  return vecClampAbs(vecMult(fromOtherPoint, repelConst/dist**3), 0, maxRepel)
+}
+const getSpringForce = (point, otherPoint, optimalDistance, springConst=50) => vecMult(vecNorm(vecSub(point, otherPoint)), springConst * (1 - distance(point, otherPoint) / optimalDistance))
+
+export const relaxVertices = (vertices, optimalDistancesMap, gravityCenter, timeStep=0.1) => {
+  const meanCoords = vecMean(vertices)
+  const gravity = getLinearFravityForce(meanCoords, gravityCenter)
+  const newVertices = vertices.map((v, idx) => {
+    let sumForce = gravity
+    vertices.forEach((ov, ovIdx) => {
+      if (ov == v) return
+      const repelForce = getRepelForce(v, ov)
+      sumForce = vecAdd(sumForce, repelForce)
+      const optimalDistance = optimalDistancesMap[idx][ovIdx] || null
+      if (optimalDistance) {
+        const springForce = getSpringForce(v, ov, optimalDistance)
+        sumForce = vecAdd(sumForce, springForce)
+      }
+    })
+    return vecAdd(v, vecMult(sumForce, timeStep))
+  })
+  return newVertices
+}
